b5db3bf52683ad1ec9bef5e0b837a6cb7a95cff7
Damien Senger <damien@raccoon.studio>
2017-04-03 21:09:46 +0200

    misc: CitizenKey WebBundle moved to a specific repository

--- src/CitizenKey/WebBundle/Tests/Controller/PhoneControllerTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace CitizenKey\WebBundle\Tests\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-
-class PhoneControllerTest extends WebTestCase
-{
-}
--- src/CitizenKey/WebBundle/WebBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace CitizenKey\WebBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class WebBundle extends Bundle
-{
-}
